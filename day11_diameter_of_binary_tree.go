6b75e849e1fb8bfe567194f29dba412a91e75f75
monirz <monir.smith@gmail.com>
2020-04-11 20:24:22 +0600

    day11 problem solved

---
+++ day11_diameter_of_binary_tree.go
@@ -0,0 +1,44 @@
+package main 
+
+// type TreeNode struct {
+// 	     Val int
+// 	     Left *TreeNode
+// 	     Right *TreeNode
+// }
+
+
+func diameterOfBinaryTree(root *TreeNode) int {
+   if root == nil {
+	   return 0 
+   }   
+
+   lHeight := height(root.Left)
+   rHeight := height(root.Right)
+
+   lDiameter := diameterOfBinaryTree(root.Left)
+   rDiameter := diameterOfBinaryTree(root.Right)
+
+   return max(lHeight + rHeight, max(lDiameter, rDiameter))
+
+}
+
+
+func height(tree *TreeNode)int {
+    if tree == nil {
+		return 0
+	}
+
+	return 1 + max(height(tree.Left), height(tree.Right))
+}
+
+func max(a,b int)int{
+
+	if a > b {
+		return a 
+	}
+
+	return b 
+}
+
+
+
